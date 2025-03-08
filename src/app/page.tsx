a05ebf2ad3e0db138f165c3bc0b6491fb0c5f61a
jostan30 <mathiasjostan@gmail.com>
2025-03-08 16:40:36 +0530

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const ApiCheck = await api.post.T3();
+  void api.post.getLatest.prefetch();
+
+  return (
+    <div className="flex-col">
+      <div>{hello?.greeting}</div>
+      <div>{ApiCheck?.annonce}</div>
+    </div>
+  );
+}
